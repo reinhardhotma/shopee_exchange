ffd82720b535dfbb138b1315b7e58a79c6a63aaa
reinhardhotma <reinhard_hotma@yahoo.com>
2019-04-07 13:24:52 +0700

    add documentation

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ get '/' do
   "Shopee!"
 end
 
+# User wants to add an exchange rate to the list
+### This will add new exchange rate to the list
+### Params:
+###  - from (String): Currency to convert from
+###  - to (String): Currency to convert to
 post '/new_conversion' do
   from = params[:from]
   to = params[:to]
@@ -14,6 +19,13 @@ post '/new_conversion' do
   }.to_json
 end
 
+# User wants to input daily exchange rate data
+### This will add new rate to the exchange
+### Params:
+###  - from (String): Currency to convert from
+###  - to (String): Currency to convert to
+###  - date (String): Selected date
+###  - rate (float): Rate of the conversion
 post '/new_rate' do
   from = params[:from]
   to = params[:to]
@@ -29,6 +41,10 @@ post '/new_rate' do
   }.to_json
 end
 
+# User has a list of exchange rates to be tracked
+### This will give the summary of currencies on the selected date
+### Params:
+###  - date (String): Selected date
 post '/exchange_rates' do
   date = params[:date].to_date
   payload = []
@@ -54,6 +70,11 @@ post '/exchange_rates' do
   payload.to_json
 end
 
+# User wants to see the exchange rate trend from the most recent 7 data points
+### This will give the rate trend from the last 7 days, along with the average and variance
+### Params:
+###  - from (String): Currency to convert from
+###  - to (String): Currency to convert to
 post '/daily_rate' do
   date = Date.today
   from = params[:from]
@@ -79,6 +100,11 @@ post '/daily_rate' do
   }.to_json
 end
 
+# User wants to remove an exchange rate from the list
+### Used to remove an exchange rate
+### Params:
+###  - from (String): Currency to convert from
+###  - to (String): Currency to convert to
 post '/delete_rate' do
   from = params[:from]
   to = params[:to]
@@ -89,6 +115,7 @@ post '/delete_rate' do
   }.to_json
 end
 
+# Helper method to count the average rates
 def count_avg(rates)
   sum = 0
   rates.each do |rate|
